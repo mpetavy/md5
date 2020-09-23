445a7cca8e3f324e2e84e12a2cdef1903744e44f
mpetavy <mpetavy>
2020-09-24 00:01:40 +0200

    multiple filenames

--- main.go
+++ main.go
@@ -13,62 +13,68 @@ import (
 )
 
 var (
-	filename *string
+	filenames common.MultiValueFlag
 	hashAlg  *string
 )
 
 func init() {
 	common.Init(false, "1.0.0", "2017", "simple http tool", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, run, 0)
 
-	filename = flag.String("f", "", "filename to hash, '.' for STDIN")
+	flag.Var(&filenames,"f","filename(s) to hash, '.' for STDIN")
 	hashAlg = flag.String("h", "md5", "hash algorithmn (md5,sha224,sha256)")
 }
 
 func run() error {
-	b, err := common.FileExists(*filename)
-	if common.Error(err) {
-		return err
-	}
-
-	if !b {
-		return &common.ErrFileNotFound{FileName: *filename}
-	}
-
-	var hasher hash.Hash
-
-	switch *hashAlg {
-	case "":
-	case "md5":
-		hasher = md5.New()
-	case "sha224":
-		hasher = sha256.New224()
-	case "sha256":
-		hasher = sha256.New()
-	default:
-		return fmt.Errorf("unknown hash algorithm: %s", *hashAlg)
-	}
-
-	var file *os.File
-
-	if *filename == "." {
-		file = os.Stdin
-	} else {
-		file, err = os.Open(*filename)
+	for _,filename := range filenames {
+		b, err := common.FileExists(filename)
 		if common.Error(err) {
 			return err
 		}
 
-		defer func() {
-			common.Error(file.Close())
-		}()
-	}
+		if !b {
+			return &common.ErrFileNotFound{FileName: filename}
+		}
 
-	_, err = io.Copy(hasher, file)
-	if common.Error(err) {
-		return err
-	}
+		var hasher hash.Hash
 
-	fmt.Printf("%s\n", hex.EncodeToString(hasher.Sum(nil)))
+		switch *hashAlg {
+		case "":
+		case "md5":
+			hasher = md5.New()
+		case "sha224":
+			hasher = sha256.New224()
+		case "sha256":
+			hasher = sha256.New()
+		default:
+			return fmt.Errorf("unknown hash algorithm: %s", *hashAlg)
+		}
+
+		var file *os.File
+
+		if filename == "." {
+			file = os.Stdin
+		} else {
+			file, err = os.Open(filename)
+			if common.Error(err) {
+				return err
+			}
+
+			defer func() {
+				common.Error(file.Close())
+			}()
+		}
+
+		_, err = io.Copy(hasher, file)
+		if common.Error(err) {
+			return err
+		}
+
+		if !*common.FlagNoBanner {
+			fmt.Printf("%s: %s\n", filename,hex.EncodeToString(hasher.Sum(nil)))
+		} else {
+			fmt.Printf("%s\n", hex.EncodeToString(hasher.Sum(nil)))
+		}
+	}
 
 	return nil
 }
